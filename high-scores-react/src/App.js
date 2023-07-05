5ed17b59080f14b8466e6e66f5a4f30ed6270434
Saliha Popal <salihapopal@gmail.com>
2023-07-05 21:40:38 +0100

    Level 4
    Using useState in order to toggles the sort order of every high-score-table between ascending and descending by scores.

--- high-scores-react/src/App.js
+++ high-scores-react/src/App.js
@@ -1,13 +1,21 @@
-//          level 1 and 2
-import React from 'react';
+
+import React, { useState } from 'react';
 import './App.css';
 import data from './scores.json';
 
 const HighScoreTable = () => {
+  const [ascendingSort, setAscendingSort] = useState("ascending");
+  const changeSortOrder = () => {
+     setAscendingSort(!ascendingSort);
+  }
+
   const sortedCountryNames = data.sort((a, b) => a.name.localeCompare(b.name));
-  
+
   return (
     <div className="table-content">
+      <button onClick={changeSortOrder}>
+        Change Sort Order: {ascendingSort ? "Ascending" : "Descending"}
+      </button>
       <table>
         <tbody>
           {sortedCountryNames.map((country, index) => (
@@ -15,7 +23,9 @@ const HighScoreTable = () => {
               <tr className="country-row">
                 <td colSpan="3">{`High Scores: ${country.name}`}</td>
               </tr>
-              {country.scores.sort((a, b) => b.s - a.s).map((score, scoreIndex) => (
+              {country.scores
+              .sort((a, b) => (ascendingSort ? a.s 
+                 - b.s :  b.s - a.s )).map((score, scoreIndex) => (
                 <tr key={scoreIndex}>
                   <td colSpan="1">{score.n}</td>
                   <td>{score.s}</td>
@@ -30,10 +40,15 @@ const HighScoreTable = () => {
 };
 
 const App = () => {
+  const [descendingSort, setDescendingSort] = useState("descending");
+
+  const changeSortDirection = () => {
+    setDescendingSort(descendingSort === "ascending" ? "descending" : "ascending")
+  }
   return (
     <div className="App">
       <h1>High Scores per Country</h1>
-      <HighScoreTable />
+      <HighScoreTable sortDirection={changeSortDirection} />
     </div>
   );
 };
